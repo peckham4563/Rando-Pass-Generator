25adb2050a8f52f32aec3812ed1fdbde4513e6b7
unknown <peckham4563@gmail.com>
2022-11-10 21:25:27 -0600

    Code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 // Assignment code here
+function passGen(len) {
+var text = '';
 
+for (var i = 0; i < len; i++)
+{
+let min = 8;
+let max = 128;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+var charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' + ' !"#$%&()*+,-./;:<>=?@{}[]^_`|~\'';
+
+return text;
+}
+
+console.log(charset)
+console.log(passGen(8))
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
